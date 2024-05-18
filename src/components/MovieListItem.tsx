d7e4824e7862dfce61e6fb2cfc7e5faf4a72fffa
afaqahmedkhan <afaq.qwe@gmail.com>
2024-05-18 17:12:24 +0200

    Added a fallback image for movies without a poster

--- src/components/MovieListItem.tsx
+++ src/components/MovieListItem.tsx
@@ -4,6 +4,8 @@ import Link from 'next/link';
 import React from 'react';
 import { Button } from './ui/button';
 
+const image = '/fallback-image.png';
+
 interface MovieListItemProps {
   movie: Movie;
 }
@@ -12,12 +14,17 @@ function MovieListItem({ movie }: MovieListItemProps) {
   return (
     <Card className="p-4 bg-gray-600 text-white flex-1">
       <CardHeader className="pb-2">
-        <CardTitle className="text-lg">{movie.Title}</CardTitle>
+        <CardTitle className="text-lg w-40">{movie.Title}</CardTitle>
       </CardHeader>
       <CardContent className="flex flex-col items-center">
         <Link href={`/movie/${movie.imdbID}`}>
           <p className="text-sm">{movie.Year}</p>
-          <img src={movie.Poster} alt={movie.Title} className="h-64 object-cover mt-2 rounded" />
+          <img src={movie.Poster}
+            onError={(e) => {
+              (e.target as HTMLImageElement).src = image;
+            }}
+            alt={movie.Title}
+            className="w-48 h-64 object-cover mt-2 rounded" />
         </Link>
         <Button variant="outline" className='my-2 text-black bg-amber-300 hover:bg-amber-200'>
           <a href={`https://www.imdb.com/title/${movie.imdbID}`} target="_blank" rel="noopener noreferrer">
